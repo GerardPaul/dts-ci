eaefd7b5f6858372a9a70ebbcaab86982f5ff683
Gerard Paul P. Labitad <gerardpaul.labitad@outlook.com>
2014-09-28 15:49:45 +0800

    Restored document page for admin.

--- application/views/admin/rd_documents.php
+++ application/views/admin/rd_documents.php
@@ -1,11 +1,11 @@
 <div class="container">
-<div class="row">
-	<div class="col-xs-12">
-<?php 
-if ($documents !== FALSE) {
+    <div class="row">
+        <div class="col-xs-12">
+            <?php
+            if ($documents !== FALSE) {
 
-		//Create the HTML table header
-		echo <<<HTML
+                //Create the HTML table header
+                echo <<<HTML
 
 		<table class="table table-condensed table-striped table-responsive table-hover display" id="documentsTable">
 			<thead>
@@ -19,23 +19,26 @@ if ($documents !== FALSE) {
 			</thead>
 			<tbody>
 HTML;
-		//Do we have an array of users or just a single user object?
-		if (is_array($documents) && count($documents)) {
-			//Loop through all the users and create a row for each within the table
-			foreach ($documents as $document) {
-				$status = '';
-				$stat = $document->getStatus();
-				if($stat=='Cancelled') $status = '<span class="text-danger status"><i class="glyphicon glyphicon-remove-sign" title="'.$stat.'" data-toggle="tooltip"></i></span>';
-				else if($stat=='On-Going') $status = '<span class="text-warning status"><i class="glyphicon glyphicon-info-sign" title="'.$stat.'" data-toggle="tooltip"></i></span>';
-				else if($stat=='Compiled') $status = '<span class="text-success status"><i class="glyphicon glyphicon-ok-sign" title="'.$stat.'" data-toggle="tooltip"></i></span>';
-				
-				$viewLink = base_url('admin/document/details/'.$document->getId());
-				
-				$subject = $document->getSubject();
-				if($document->getMarkReceived()=='0')
-					$subject = '<strong>'.$document->getSubject().'</strong>';
-				
-				echo <<<HTML
+                //Do we have an array of users or just a single user object?
+                if (is_array($documents) && count($documents)) {
+                    //Loop through all the users and create a row for each within the table
+                    foreach ($documents as $document) {
+                        $status = '';
+                        $stat = $document->getStatus();
+                        if ($stat == 'Cancelled')
+                            $status = '<span class="text-danger status"><i class="glyphicon glyphicon-remove-sign" title="' . $stat . '" data-toggle="tooltip"></i></span>';
+                        else if ($stat == 'On-Going')
+                            $status = '<span class="text-warning status"><i class="glyphicon glyphicon-info-sign" title="' . $stat . '" data-toggle="tooltip"></i></span>';
+                        else if ($stat == 'Compiled')
+                            $status = '<span class="text-success status"><i class="glyphicon glyphicon-ok-sign" title="' . $stat . '" data-toggle="tooltip"></i></span>';
+
+                        $viewLink = base_url('admin/document/details/' . $document->getId());
+
+                        $subject = $document->getSubject();
+                        if ($document->getMarkReceived() == '0')
+                            $subject = '<strong>' . $document->getSubject() . '</strong>';
+
+                        echo <<<HTML
 
 					<tr>
 						<td>{$document->getFrom()}</td>
@@ -48,24 +51,22 @@ HTML;
 					</tr>
 
 HTML;
-			}
-
-		} 
-		//Close the table HTML
-		echo <<<HTML
+                    }
+                }
+                //Close the table HTML
+                echo <<<HTML
 			</tbody>
 		</table>
 HTML;
-
-	} else {
-		//Now user could be found so display an error messsage to the user
-		echo <<<HTML
+            } else {
+                //Now user could be found so display an error messsage to the user
+                echo <<<HTML
 
 			<div class="alert alert-warning">There are no <strong>Documents</strong> to display.</div>
 
 HTML;
-	}
-?>
-	</div>
-</div>
+            }
+            ?>
+        </div>
+    </div>
 </div>
